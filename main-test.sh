0e836ac7b54c108eff7789832a79bc8b77f18e78
arnarsnaeland <addisnae@gmail.com>
2024-07-08 16:44:35 +0200

    testing

--- main-test.sh
+++ main-test.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=sm3090el8
 #SBATCH -N 1      # Minimum of 1 node
 #SBATCH -n 8     # 8 MPI processes per node
-#SBATCH --time=6-00:00:00
+#SBATCH --time=0-00:15:00
 #SBATCH --gres=gpu:RTX3090:1 # Request 1 GPU (can increase for more)
 module load Python/3.11.3-GCCcore-12.3.0
 source ~/crystal-llm/bin/activate
@@ -11,5 +11,4 @@ export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 export OMP_NUM_THREADS=1
 export OPENBLAS_NUM_THREADS=1
-cd ~/master-thesis
-python main.py
+python ~/master-thesis/main.py
--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from ocdata.core import Bulk, Adsorbate
+from ocdata.core import Bulk, Adsorbate, Slab, AdsorbateSlabConfig
 from ase import Atoms
 from ase.io import read
 from modelPrompter import prompt_llm
 
+# Define the Miller index for the surface (just for testing, llm will generate this)
+MILLER_IDX = (1, 1, 1)
 
-cifs = prompt_llm("7b", "exp/7b-test-run/checkpoint-84000")
+cifs = prompt_llm("7b", "~/crystal-text-llm/exp/7b-test-run/checkpoint-84000")
 
 
 for cif in cifs:
     bulk = Bulk(read(cif.cif, ':', 'cif')[0])
+    print("Bulks:")
     print(bulk)
+    adsorbate = Adsorbate()
+    print("Adsorbate:")
+    print(adsorbate)
+    slabs = Slab.from_bulk_get_specific_millers(bulk, MILLER_IDX)
+    print("Slabs:")
+    print(slabs)
+    random_adslabs = AdsorbateSlabConfig(slabs[0], adsorbate, mode="random_site_heuristic_placement", num_sites=100)
+    print("Random Adsorbate Slab Config:")
+    print(random_adslabs)
+
+
+
 

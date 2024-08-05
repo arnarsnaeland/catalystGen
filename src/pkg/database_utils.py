e8d8d3867fa6d57f062f8b9f02c8eba8dfe3d1ed
arnarsnaeland <addisnae@gmail.com>
2024-08-05 17:26:58 +0000

    4

--- src/pkg/database_utils.py
+++ src/pkg/database_utils.py
@@ -15,10 +15,13 @@ def write_slabs_to_db(slabs, db):
             
 def write_adsorbate_slab_configs_to_db(adsorbate_slab_configs, db, relaxed):
     with db as db:
-        for adsorbate_slab_config in adsorbate_slab_configs:
-            for atom_obj in adsorbate_slab_config.atoms_list:
-                if relaxed:
-                    id = db.write(atom_obj, bulk_id=adsorbate_slab_config.slab.bulk.db_id, slab_id=adsorbate_slab_config.slab.db_id, not_relaxed_id=atom_obj.db_id, relaxed=relaxed)
-                else:
+        if relaxed:
+            for adsorbate_slab_config in adsorbate_slab_configs:
+                for atom_obj in adsorbate_slab_config.atoms_list:
+                    id = db.write(atom_obj, bulk_id=adsorbate_slab_config.slab.bulk.db_id, slab_id=adsorbate_slab_config.slab.db_id, relaxed=relaxed, relaxed_id=adsorbate_slab_config.not_relaxed_id)
+                    atom_obj.db_id = id
+        else:    
+            for adsorbate_slab_config in adsorbate_slab_configs:
+                for atom_obj in adsorbate_slab_config.atoms_list:
                     id = db.write(atom_obj, bulk_id=adsorbate_slab_config.slab.bulk.db_id, slab_id=adsorbate_slab_config.slab.db_id, relaxed=relaxed)
                     atom_obj.db_id = id
